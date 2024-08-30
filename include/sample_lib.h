482b60b68af1717828e362f098b86cbdf08e3874
ntargon <argon19951128@gmail.com>
2024-08-31 08:29:53 +0900

    init

---
+++ include/sample_lib.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+int32_t add(int32_t x, int32_t y);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
---
+++ src/sample_lib.cpp
@@ -0,0 +1,6 @@
+#include "sample_lib.h"
+
+int32_t add(int32_t x, int32_t y)
+{
+    return x + y;
+}
